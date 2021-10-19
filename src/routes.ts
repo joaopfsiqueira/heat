52a6486321e1b8b241c239094e1c658d31607b6e
João Pedro Siqueira <joaopfsiqueiraa@gmail.com>
2021-10-19 13:03:30 -0300

    first commit

---
+++ src/routes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express"; 
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController"
+import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
+import { CreateMessageController } from "./controllers/CreateMessageController";
+
+const router = Router();
+
+router.post("/authenticate", new AuthenticateUserController().handle);
+
+router.post("/messages", ensureAuthenticated , new CreateMessageController().handle);
+
+export { router };
---
+++ src/services/createMessageService.ts
@@ -0,0 +1,21 @@
+import prismaClient from "../prisma"
+ 
+class createMessageService {
+    async execute(text: string, user_id: string){
+    const message = await prismaClient.message.create({
+        data: {
+            text,
+            user_id
+        },
+        include:{
+            user:true
+        }
+    })
+
+    return message;
+}
+}   
+
+export {
+    createMessageService,
+}
